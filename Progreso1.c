f1466dab530a21180ceec601ca54dd4922c67470
IngGiova <giovanny.padilla@udla.edu.ec>
2023-11-01 15:47:24 -0500

    proceso para calcular el hexadecimal de un numero

--- Progreso1.c
+++ Progreso1.c
@@ -1,7 +1,13 @@
 #include<stdio.h>
 
 int main(){
-    int num;
-    printf("Ingrese un numero entero:");
+    int num,hexa;
+    //se pide al usuario ingresar el numero entero//
+    printf("Ingrese un numero entero:\n");
+    scanf("%d", &num);
+    //creacion del codigo para calcular el hexadecimal de un numero//
+    hexa = num%6;
+    printf("El hexadecimal del numero es:%d", hexa);
+
     return 0;
 }

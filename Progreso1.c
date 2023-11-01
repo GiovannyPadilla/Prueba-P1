ead65c3b0083f37032a5a8f15ffce12b2f9640cc
IngGiova <giovanny.padilla@udla.edu.ec>
2023-11-01 16:29:42 -0500

    Usamos %x para leer el num en hexadecimal

--- Progreso1.c
+++ Progreso1.c
@@ -1,17 +1,19 @@
+//Construya un programa que reciba como dato de entrada un número entero positivo, n, y regrese como dato de salida la representación de este número en hexadecimal.//
 #include<stdio.h>
 
 int main(){
-    int num,hexa;
+    int num;
     //se pide al usuario ingresar el numero entero//
     printf("Ingrese un numero entero:\n");
     scanf("%d", &num);
     //creacion del codigo para calcular el hexadecimal de un numero//
     if (num>0)
     {
-        hexa = num%6;
-    }
         
-    printf("El hexadecimal del numero es:%d", hexa);
-
+        printf("El hexadecimal de un numero es:%x", num);
+        
+    }else{
+       printf("El numero ingresado debe ser entero");
+    }
     return 0;
 }
